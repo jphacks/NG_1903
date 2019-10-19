521b0ccf52e9713ef04da722eef173de095104c2
Takamichi Omori <merrytakamerry@gmail.com>
2019-10-20 00:14:07 +0900

    add arrow.css

--- front/src/pages/TeamDetailPage.tsx
+++ front/src/pages/TeamDetailPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { RouteComponentProps, withRouter } from 'react-router-dom'
+import "./arrow.css"
 
 const TeamDetailPage: React.FC<RouteComponentProps>  = props => {
     return (
-        <div>
-            TeamDetail
-        </div>
+        <div className="arrow" onClick={() => props.history.push("/")} />
     )
 }
 
--- front/src/pages/UserDetail.tsx
+++ front/src/pages/UserDetail.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { RouteComponentProps, withRouter } from 'react-router-dom'
+import "./arrow.css";
 
 const UserDetail: React.FC<RouteComponentProps>  = props => {
     return (
         <div>
-            UserDetail
+            <div className="arrow" onClick={() => props.history.push("/")} />
         </div>
     )
 }
