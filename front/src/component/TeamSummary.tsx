5d59580ea3137f3eeb42e6ca97b7ea03f5f3aa9d
Takamichi Omori <merrytakamerry@gmail.com>
2019-10-19 14:29:12 +0900

    add redux

--- front/src/component/TeamSummary.tsx
+++ front/src/component/TeamSummary.tsx
@@ -1,20 +1,69 @@
 import React from "react";
 import {  teamDetail } from '../types';
+import { Doughnut } from 'react-chartjs-2'
 
 type Props = {
     teamData: teamDetail;
 };
+type DataType = {
+    labels: Array<string>;
+    datasets: Array<DatasetType> 
+}
+
+type DatasetType = {
+    data: Array<number>;
+    backgroundColor : Array<string>;
+    hoverBackgroundColor: Array<string>;
+}
+// const data = {
+// 	labels: [
+// 		'Red',
+// 		'Green',
+//         'Yellow',
+// 	],
+// 	datasets: [{
+// 		data: [300, 50, 100],
+		// backgroundColor: [
+		// '#FF6384',
+		// '#36A2EB',
+        // '#FFCE56',
+		// ],
+		// hoverBackgroundColor: [
+		// '#FF6384',
+		// '#36A2EB',
+		// '#FFCE56'
+		// ]
+// 	}]
+// };
 
 const TeamSummary: React.FC<Props> = props => {
+    const data: DataType = {
+        labels:[],
+        datasets:[{
+            data:[],
+            // backgroundColor: [],
+            // hoverBackgroundColor: []
+            backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56',
+            ],
+            hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+            ]
+            }
+        ]
+    }
+    props.teamData.teamMember.map(d => {
+        data.labels.push(d.userName)
+        data.datasets[0].data.push(d.userData.weeklyDistance)
+    })
     return (
         <section onClick={() => console.log("clicked!")}>
             <p>チーム目標: {props.teamData.teamGoal}</p>
-            {props.teamData.teamMember.map(data => (
-                <div>
-                    <p>{data.userName}</p>
-                    <p>{data.userData.weeklyDistance}</p>
-                </div>
-            ))}
+            <Doughnut data={data}/>
         </section>
     );
 };
